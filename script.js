08b0af5e435ccb4a10d32752fe33a18466bc50b9
knva65 <91244269+knva65@users.noreply.github.com>
2022-01-23 13:48:01 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function ChangeMe1() {
+    let img_1 = document.getElementById ('img_1');
+    img_1.src ="/assets/images/2.jpg";
+}
+
+function ChangeMe2() {
+    let img_1 = document.getElementById ('img_1');
+    img_1.src ="/assets/images/3.jpg";
+}
